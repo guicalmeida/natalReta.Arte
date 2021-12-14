6edbe9dff2e37f4a1ef51ba248e33a5a99c5d604
Guilherme Almeida <guilherme.almeida@collact.com.br>
2021-12-14 10:49:45 -0300

    fix: variable algorithm implemented

--- build/build.js
+++ build/build.js
@@ -1,19 +1,20 @@
-var nCols = 6;
-var nRows = 6;
+var nCols = 51;
+var nRows = 51;
 var currentAngle = 0;
 var targetAngle = 0;
+var lineThickness = 9;
+var canvasSize = 1200;
+var gridXCenterCoords = [];
 var vLines = [];
 var hLines = [];
 function setup() {
     noStroke();
-    var canvasSize = 1200;
     createCanvas(canvasSize, canvasSize);
     angleMode(DEGREES);
     setInterval(function () { return targetAngle += 90; }, 2500);
     var centerPoints = get2DGridCenterPoints(nRows, nCols);
-    var xCoords = [100, 300, 500, 700, 900, 1100];
     centerPoints.forEach(function (centerPoint) {
-        xCoords.indexOf(centerPoint[0]) % 2 === 0 ?
+        gridXCenterCoords.indexOf(centerPoint[0]) % 2 === 0 ?
             vLines.push(createVector(centerPoint[0], centerPoint[1])) :
             hLines.push(createVector(centerPoint[0], centerPoint[1]));
     });
@@ -26,6 +27,7 @@ function get2DGridCenterPoints(rows, cols) {
     var coordArray = [];
     for (var i = 1; i <= rows; i++) {
         shapeCenterX = gridUnitWidth * i - gridUnitWidth / 2;
+        gridXCenterCoords.push(shapeCenterX);
         for (var j = 1; j <= cols; j++) {
             shapeCenterY = gridUnitHeight * j - gridUnitHeight / 2;
             coordArray.push([shapeCenterX, shapeCenterY]);
@@ -44,14 +46,14 @@ function draw() {
         push();
         translate(vector.x, vector.y);
         rotate(currentAngle);
-        rect(0, 0, 9, height / 6);
+        rect(0, 0, lineThickness, height / nRows);
         pop();
     });
     hLines.forEach(function (vector) {
         push();
         translate(vector.x, vector.y);
         rotate(currentAngle);
-        rect(0, 0, width / 6, 9);
+        rect(0, 0, width / nCols, lineThickness);
         pop();
     });
 }
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,7 +1,11 @@
-let nCols = 6;
-let nRows = 6;
+let nCols = 51;
+let nRows = 51;
 let currentAngle = 0;
 let targetAngle = 0;
+
+let lineThickness = 9;
+let canvasSize = 1200;
+let gridXCenterCoords: number[] = [];
 
 
 const vLines: p5.Vector[] = [];
@@ -9,61 +13,60 @@ const hLines: p5.Vector[] = [];
 
 function setup() {
   noStroke();
-  let canvasSize = 1200;
   createCanvas(canvasSize, canvasSize);
   angleMode(DEGREES);
-  setInterval(()=> targetAngle+=90, 2500);
-  
+  setInterval(() => targetAngle += 90, 2500);
+
   const centerPoints = get2DGridCenterPoints(nRows, nCols);
-  const xCoords = [100, 300, 500, 700, 900, 1100];
   centerPoints.forEach((centerPoint) => {
-    xCoords.indexOf(centerPoint[0]) % 2 === 0  ?
-    vLines.push(createVector(centerPoint[0], centerPoint[1])) :
-    hLines.push(createVector(centerPoint[0], centerPoint[1]))
+    gridXCenterCoords.indexOf(centerPoint[0]) % 2 === 0 ?
+      vLines.push(createVector(centerPoint[0], centerPoint[1])) :
+      hLines.push(createVector(centerPoint[0], centerPoint[1]))
   })
 }
 
 function get2DGridCenterPoints(rows: number, cols: number): number[][] {
   let shapeCenterX = 0;
-  let gridUnitWidth = width/rows;
-  
-  let shapeCenterY= 0;
-  let gridUnitHeight = height/cols;
-  
+  let gridUnitWidth = width / rows;
+
+  let shapeCenterY = 0;
+  let gridUnitHeight = height / cols;
+
   const coordArray = [];
-  
+
   for (let i = 1; i <= rows; i++) {
-    shapeCenterX = gridUnitWidth * i - gridUnitWidth/2;
+    shapeCenterX = gridUnitWidth * i - gridUnitWidth / 2;
+    gridXCenterCoords.push(shapeCenterX);
     for (let j = 1; j <= cols; j++) {
-      shapeCenterY = gridUnitHeight * j - gridUnitHeight/2;
+      shapeCenterY = gridUnitHeight * j - gridUnitHeight / 2;
       coordArray.push([shapeCenterX, shapeCenterY]);
     }
   }
-  
+
   return coordArray;
-  
+
 }
 
 function draw() {
   background(255);
   fill(1);
   rectMode(CENTER)
-  if(currentAngle < targetAngle){
+  if (currentAngle < targetAngle) {
     currentAngle++
   }
-  vLines.forEach( vector => {
+  vLines.forEach(vector => {
     push()
     translate(vector.x, vector.y)
     rotate(currentAngle)
-    rect(0, 0, 9, height/6)
+    rect(0, 0, lineThickness, height / nRows)
     pop()
   })
-  
+
   hLines.forEach(vector => {
     push()
     translate(vector.x, vector.y)
     rotate(currentAngle)
-    rect(0, 0, width/6, 9)
+    rect(0, 0, width / nCols, lineThickness)
     pop()
   })
 }
